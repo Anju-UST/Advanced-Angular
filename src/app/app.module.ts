4bef5b8c572a0dc4433e91ef1c7da3873931fa39
Anju-UST <Anju.Reji@ust.com>
2023-06-10 09:57:11 +0530

    communication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { VoteTakerComponent } from './vote-taker/vote-taker.component';
+import { VoterComponent } from './voter/voter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VoteTakerComponent,
+    VoterComponent
   ],
   imports: [
     BrowserModule
